88077074834069d33cd06d485841aa6c56a36088
Chris Chu <42784131+wanderingessence@users.noreply.github.com>
2019-06-24 13:48:28 -0500

    Wrote pseudocode for question 12

---
+++ Linked Lists/q12.java
@@ -0,0 +1,13 @@
+//Implement an algorithm to delete a node in the middle of a singly linked list, given only access
+//to that node. 
+public class q12 {
+
+	public static void main(String[] args) {
+		//Since we can only access and change the value of this node, we cannot delete it
+		//this we need to set this node to the next node, then delete the next node.
+		//the following code is as follows:
+		//n.data() = n.next.data()
+		//n.next() = n.next.next();
+	}
+
+}
