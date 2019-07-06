6f4618543ac54036f2499db64090b77b6986b489
Chris Chu <42784131+wanderingessence@users.noreply.github.com>
2019-07-06 13:27:20 -0500

    Started implementing the Stacks and Queues Interface

---
+++ Stacks and Queues/q18.java
@@ -0,0 +1,17 @@
+//Describe how you can use a single array to implement three stacks 
+public class q18 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//1. Generate a very large array of size that is a multiple of three. Let's
+		//say the size of the array is 300.
+		
+		//The indices would be 0, 99, and 199 in this instance. Let's say we add part to a stack
+		//you pick which stack based on which indices to start in, and then to add, just push to the next index
+		//and update the top pointer. To delete, select a list, then pull the top pointer -1.
+		//to peek, find the index of the pointer stored at the program then update the pointer. 
+
+	}
+
+}
