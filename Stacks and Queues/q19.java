fe2b90a2059949ea47ffa69c51df34071db153d9
Chris Chu <42784131+wanderingessence@users.noreply.github.com>
2019-07-06 13:35:07 -0500

    Solved q19

---
+++ Stacks and Queues/q19.java
@@ -0,0 +1,11 @@
+//How would you design a stack which has a function min that has the minimum element.
+public class q19 {
+
+	public static void main(String[] args) {
+		// Push already operates in O(1) time. Pop also happens in O(1) time. 
+		//Every time you push a stack, it also has a minimum element. If less, update the minimum element
+		//else, the minimum element is the minimum element obtained in the remainder of the stack. 
+
+	}
+
+}
